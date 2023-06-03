18a14843cf8215ef4e53b0f8b7bfbeda73ee5abb
aniketghavte <aniketghavte@gmail.com>
2023-06-03 15:31:02 +0530

    new component

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
           content="actualone, actual01, actual 101, collaboration, bounty, protocol"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preload" href="path/to/karla-font.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
