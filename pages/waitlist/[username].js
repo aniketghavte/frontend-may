99d7134ed8fa0020852d9348795821b41dd1b5cd
Aniket <aniketghavte@gmail.com>
2024-02-18 20:41:28 +0530

    home page

--- pages/waitlist/[username].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
- 
-export default function Page() {
-  const router = useRouter();
-  return <div>
-        Post: {router.query.username}
-    
-    </div>;
-}
